1dd6f9ef69162ffea6d5344bf847d11c85afbc93
Kwanton :) <quentibiew@gmail.com>
2025-07-04 21:14:41 +0200

    can now add md files and yt videos

---
+++ docs/zombie-ai.md
@@ -0,0 +1,14 @@
+## Goal
+The goal of this project was to make an AI using either an FSM or a behavior tree. As I personally felt like a behavior tree offered more flexibility I used that.
+
+```cpp
+#include <iostream>
+
+// Entry point
+int main() {
+    std::cout << "Hello, C++ world!" << std::endl;
+    return 0;
+}
+```
+
+kaas
--- projectDetail.js
+++ projectDetail.js
@@ -1,7 +1,7 @@
 
 class ProjectDetail extends HTMLElement {
   static get observedAttributes() {
-    return ['title','description','thumbnail','github','itch'];
+    return ['title','description','thumbnail','github','itch', 'markdown', 'video'];
   }
   constructor() {
     super();
@@ -30,6 +30,54 @@ class ProjectDetail extends HTMLElement {
     a.href = value;
     a.classList.toggle('hidden', !value);
   }
+
+    if (name === 'video') {
+  const iframe = root.querySelector('#video-iframe');
+  if (value) {
+    // extract ID from various URL formats
+    let id;
+    if (value.includes('youtu.be/')) {
+      id = value.split('youtu.be/')[1].split(/[?&]/)[0];
+    } else if (value.includes('watch?v=')) {
+      id = value.split('watch?v=')[1].split('&')[0];
+    } else {
+      id = value;
+    }
+    iframe.src = `https://www.youtube.com/embed/${id}`;
+    iframe.classList.remove('hidden');
+  } else {
+    iframe.classList.add('hidden');
+  }
+}
+
+    
+    if (name === 'markdown') {
+      const mdContainer = root.querySelector('#markdown-content');
+      if (value) {
+        fetch(value)
+          .then(res => res.text())
+          .then(md => {
+
+            marked.setOptions({
+              langPrefix: 'language-',
+              highlight: (code, lang) => {
+                if (lang && hljs.getLanguage(lang)) {
+                  return hljs.highlight(code, { language: lang }).value;
+                }
+                return hljs.highlightAuto(code).value;
+              }
+            });
+
+
+            mdContainer.innerHTML = marked.parse(md);
+
+            hljs.highlightAll();
+
+          })
+          .catch(err => console.error('Could not load markdown:', err));
+      }
+    }
+
   }
 }
 
